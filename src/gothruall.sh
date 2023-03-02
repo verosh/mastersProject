7ffe19841982c70b85ef14fdfc419cc248b601a9
verosh <vero.shabun@gmail.com>
2023-03-02 14:54:00 +0000

    added facial expressions

--- src/gothruall.sh
+++ src/gothruall.sh
@@ -2,26 +2,28 @@
 
 # loop & print a folder recusively,
 print_folder_recurse() {
-    for i in "$1"/*;do
+    for i in "$1"/*;do  #before: "$1"
         if [ -d "$i" ];then
             #if the file being looked at is a directory, recursion go brrr
-
             #echo "dir: $outputpath$(basename $1)/$(basename $i)"
-            
+            #echo $i
             print_folder_recurse "$i"
-        else
+        elif [ -f "$i" ] && [ -d "$outputpath${1#$path}" ] && [[ $(basename $i) == *.csv ]]; then 
             
             #echo $outputpath${1#$path}
+
+
             #mkdir -p $outputpath${1#$path}
             #echo "starting video $i"
             #./bin/OpenPoseDemo.exe --video $i --hand --write_json $outputpath${1#$path} && continue
             #echo "finished video $i"
 
             echo $i
-            echo $1
+            cp $i $outputpath${1#$path}
+            #echo $1
             #echo $outputpath${1#$path}
             #python3 ./featureExtraction/convertDirectoryOfJsonToCsv.py $1 $outputpath${1#$path} && return
-            python3 ./featureExtraction/addingFacetouchToCsv.py $i && return
+            #python3 ./featureExtraction/addingFacetouchToCsv.py $i && return
 
             #echo "dir: $outputpath$(basename $1)/$(basename $i)"
             #echo " "
@@ -38,11 +40,11 @@ if [ -d "$1" ]; then
 else
     path="/tmp"
 fi
-
-#outputpath="$2"
-
+#path = "/home/vero/Dropbox/wellbeingdataset"
+outputpath="$2"
+#outputpath = "/home/vero/coding/masters/mastersProject/src/csvOut"
 echo "base path: $path"
-#echo "destination path: $outputpath"
+echo "destination path: $outputpath"
 print_folder_recurse $path
 
 
